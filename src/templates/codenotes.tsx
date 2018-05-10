86eed6333c609d7e3796f72c5bfc81e50626a165
Tyler Collins <tjscollins@gmail.com>
2018-05-10 13:51:45 -1000

    Fix tslint and typescript errors

--- src/templates/codenotes.tsx
+++ src/templates/codenotes.tsx
@@ -1,52 +1,54 @@
 /// <reference path="../typings/global.d.ts" />
 /// <reference path="../typings/graphql.d.ts" />
 
-import * as React from "react";
+import * as React from 'react';
 import styled from 'styled-components';
 
 import Warning from '../components/Warning';
 
-declare interface codenotesPostProps {
+declare interface CodenotesPostProps {
     data: {
         markdownRemark: markdownRemarkObject
-    } 
+    }
 }
 
-declare interface codenotesPostState {
+declare interface CodenotesPostState {
     noMathJax: boolean
 }
 
-class codenotesPost extends React.Component<codenotesPostProps, codenotesPostState> {
+class CodenotesPost extends React.Component<CodenotesPostProps, CodenotesPostState> {
 
-    constructor(props: codenotesPostProps) {
+    private mathJaxWarning = 'Warning: MathJax has failed to load correctly. ' +
+        'Mathematical content may not be displayed correctly.  You can ' +
+        'try reloading the page.';
+
+    constructor(props: CodenotesPostProps) {
         super(props);
         this.state = {
-            noMathJax: false,
+            noMathJax: false
         };
     }
 
-    private mathJaxWarning = "Warning: MathJax has failed to load correctly.  Mathematical content may not be displayed correctly.  You can try reloading the page.";
-
-    componentDidMount() {
-        const MathJax = window.MathJax;
+    public componentDidMount() {
+        const MathJax = (window as any).MathJax;
         if (MathJax) {
             MathJax.Hub.Config({
                 tex2jax: {
-                    inlineMath: [ ['$','$'], ["\\(","\\)"] ],
-                    displayMath: [ ['$$','$$'], ["\[","\]"] ],
+                    inlineMath: [ ['$', '$'], ['\\(', '\\)'] ],
+                    displayMath: [ ['$$', '$$'], ['\[', '\]'] ],
                     processEscapes: true
                 }
             });
             setTimeout(
-                () => MathJax.Hub.Queue(["Typeset", MathJax.Hub]), 
+                () => MathJax.Hub.Queue(['Typeset', MathJax.Hub]),
                 100
             );
         } else {
             this.setState({noMathJax: true});
         }
     }
-    
-    render() {
+
+    public render() {
         const { data } = this.props;
         const { noMathJax } = this.state;
         const post = data.markdownRemark;
@@ -61,7 +63,7 @@ class codenotesPost extends React.Component<codenotesPostProps, codenotesPostSta
     }
 }
 
-export default codenotesPost;
+export default CodenotesPost;
 
 const Article = styled.article`
   margin-top: 15px;
