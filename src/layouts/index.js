3e7e0dbe0f826f40979a9d0531c4129c0a966be2
Tyler Collins <tjscollins@gmail.com>
2018-05-10 14:22:11 -1000

    Adjustment to layout and redux store connection

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 import React from 'react'
 import styled, { ThemeProvider } from 'styled-components';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
+import { connect, Provider } from 'react-redux';
 
 import reducer from "../redux/reducers";
 
@@ -13,6 +13,9 @@ import NavManager from '../components/NavManager';
 import ToContents from '../components/TableOfContents';
 
 import theme from '../styles/theme';
+
+import { sidebarWidth } from '../util';
+
 
 import './index.css';
 import 'prismjs/themes/prism-okaidia.css';
@@ -32,13 +35,13 @@ const TemplateWrapper = ({ children, data }) => (
         <ThemeProvider theme={theme}>
             <div>
                 <HeaderBar />
-                <FlexContainer>
                 <ToContents 
                     pages={data.allMarkdownRemark.edges}
                     title={data.site.siteMetadata.title} />
-                <Main>
-                    {children()}
-                </Main>
+                <FlexContainer>
+                    <Main>
+                        {children()}
+                    </Main>
                 </FlexContainer>
             </div>
         </ThemeProvider>
@@ -52,20 +55,24 @@ TemplateWrapper.propTypes = {
 
 export default TemplateWrapper
 
-const FlexContainer = styled.div`
+const connectToStore = connect(state => ({expanded: state.sidebarExpanded}));
+
+const FlexContainer = connectToStore(styled.div`
   display: flex;
   flex-direction: row;
   max-width: 1600px;
+  padding-left: ${sidebarWidth};
+
+  transition: padding-left ${({theme}) => theme.sidebar.hideTransition};
 
   @media only screen and (max-width: ${({theme}) => theme.breakpoints.mobileLimit}) {
     display: block;
   }
-`
+`)
 
 const Main = styled.main`
   margin: 0 auto;
   max-width: 960px;
-  width: 100%;
   padding: 0px 1.1rem 1.45rem;
   padding-top: 0;
 `
