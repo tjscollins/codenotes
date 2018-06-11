94eee9f858b757efb6df6f069235714212f1c24c
Tyler Collins <tjscollins@gmail.com>
2018-06-11 11:44:03 -1000

    Add Skills section to resume page

--- src/components/Resume/LeftColumn.tsx
+++ src/components/Resume/LeftColumn.tsx
@@ -1,17 +1,18 @@
+/// <reference path="./resume.d.ts" />
 import * as React from 'react';
 
 import AboutSection from './About';
+import SkillsSection from './Skills';
 
-class LeftResumeColumn extends React.Component {
+class LeftResumeColumn extends React.Component<ResumeData> {
 
     public render() {
         return (
             <aside className="left-column">
                 <AboutSection {...this.props} />
-                {/* {{> about }}
-                {{> skills }}
-                {{> languages }}
-                {{> interests }} */}
+                <SkillsSection {...this.props} />
+                {/* <LanguagesSection {...this.props} /> */}
+                {/* <Interests {...this.props} /> */}
             </aside>
         );
     }
