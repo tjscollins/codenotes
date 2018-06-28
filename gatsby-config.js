40051c48151e08b7fba9bee725ebe0c69043d2be
Tyler Collins <tjscollins@gmail.com>
2018-06-27 21:45:53 -1000

    Move resume.json into dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,13 +30,6 @@ module.exports = {
         },
     },
     {
-        resolve: `gatsby-source-filesystem`,
-        options: {
-            name: `data`,
-            path: `${__dirname}/data/`,
-        },
-    },
-    {
         resolve: `gatsby-transformer-remark`,
         options: {
             plugins: [
