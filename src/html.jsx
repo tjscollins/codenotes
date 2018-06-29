adb216e6a785f50bb3510fdaebd47975d5c1ee92
Tyler Collins <tjscollins@gmail.com>
2018-06-29 10:19:51 -1000

    Add google analytics

--- src/html.jsx
+++ src/html.jsx
@@ -10,42 +10,55 @@ if (process.env.NODE_ENV === `production`) {
 }
 
 module.exports = class HTML extends React.Component {
-  render() {
-    let css
-    if (process.env.NODE_ENV === `production`) {
-      css = (
-        <style
-          id="gatsby-inlined-css"
-          dangerouslySetInnerHTML={{ __html: stylesStr }}
-        />
-      )
+
+    componentDidMount() {
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+        
+        gtag('config', 'UA-121664712-1');
     }
-    return (
-      <html {...this.props.htmlAttributes}>
-        <head>
-          <meta charSet="utf-8" />
-          <meta httpEquiv="x-ua-compatible" content="ie=edge" />
-          <meta
-            name="viewport"
-            content="width=device-width, initial-scale=1, shrink-to-fit=no"
-          />
-          {this.props.headComponents}
-          {css}
-          <script type="text/javascript" async
-            src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML">
-          </script>
-        </head>
-        <body {...this.props.bodyAttributes}>
-          {this.props.preBodyComponents}
-          <div
-            key={`body`}
-            id="___gatsby"
-            dangerouslySetInnerHTML={{ __html: this.props.body }}
-          />
-          {this.props.postBodyComponents}
-          <script defer src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"></script>
-        </body>
-      </html>
-    )
-  }
+    
+    render() {
+        let css
+        if (process.env.NODE_ENV === `production`) {
+            css = (<style
+                id="gatsby-inlined-css"
+                dangerouslySetInnerHTML={{ __html: stylesStr }}
+                />
+            )
+        }
+        return (
+            <html {...this.props.htmlAttributes}>
+                <head>
+                <meta charSet="utf-8" />
+                <meta httpEquiv="x-ua-compatible" content="ie=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width, initial-scale=1, shrink-to-fit=no"
+                />
+                {this.props.headComponents}
+                {css}
+                <script type="text/javascript" async
+                    src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.2/MathJax.js?config=TeX-MML-AM_CHTML">
+                </script>
+                </head>
+                <body {...this.props.bodyAttributes}>
+                {this.props.preBodyComponents}
+                <div
+                    key={`body`}
+                    id="___gatsby"
+                    dangerouslySetInnerHTML={{ __html: this.props.body }}
+                />
+                {this.props.postBodyComponents}
+                <script 
+                    defer 
+                    src="https://use.fontawesome.com/releases/v5.0.6/js/all.js"
+                ></script>
+                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121664712-1"></script>
+                </body>
+            </html>
+        )
+    }
 }
+
